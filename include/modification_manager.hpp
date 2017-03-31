9bec4b29687f80e772b0440df91339819e5d16d0
nuccoons <nuccoons@gmail.com>
2017-04-01 01:14:00 +0300

    Update modification_manager.hpp

--- include/modification_manager.hpp
+++ include/modification_manager.hpp
@@ -9,15 +9,14 @@ namespace gitualizer
     class ModificationManager
     {
     private:
-        Modification*   caheModification;   //Массив указателей на модификация (кэш мдифифкаций)
-        DirectoryInfo*  _dirsInfo;          //Массив указателей на директории
+        Modification*   _cacheModification;   //Массив указателей на модификация (кэш мдифифкаций)
         
     public:
-        Modification GetInitialState();             //Метод получающий начальное состояние репозитория
-        Modification GetNextModification();         //Метод получающий последующие изменения состояния репозитория
-        FileInfo GetFileInfoById(uint32_t id) const;//Метод возвращающий информацию о файле по его ID
-        int SaveToFile(const char* filename);       //Метод сохраняющий структуры Modification в файл...
-        int LoadFromFile(const char* filename);     //...и метод их загружающий из файла
+        Modification GetInitialState() const;               //Метод получающий начальное состояние репозитория
+        Modification GetNextModification() const;           //Метод получающий последующие изменения состояния репозитория
+        FileInfo GetFileInfoById(uint32_t id) const;        //Метод возвращающий информацию о файле по его ID
+        int SaveToFile(const char* filename);               //Метод сохраняющий структуры Modification в файл...
+        int LoadFromFile(const char* filename);             //...и метод их загружающий из файла
     };
 }
 
