b9941c1e95ac6faa0697a5115f9cc0d006a351d1
pspirichev <pspirichev@yandex.ru>
2017-03-31 23:32:33 +0300

    Add files via upload

--- include/internal.hpp
+++ include/internal.hpp
@@ -7,41 +7,53 @@ namespace gitualizer
 {
     struct FileInfo //Структа содержащая информацию о конкретном файле
     {
-        char*   _filename, //Имя файла в строковом виде
-                _author, //Имя автора в строковом виде
-                _message; //Сообщение с коментарием прилогающееся к фалу в строковом виде
-        int32_t _ID; //ID файла
-        char _status; //Статус файла 0 - добавлен, 1 - изменен, 2 - удален (exmpl: _status = 0;)
+        char*       _filename,  //Имя файла в строковом виде
+                    _author,    //Имя автора в строковом виде
+                    _message;   //Сообщение с коментарием прилогающееся к фалу в строковом виде
+        uint32_t    _ID;        //ID файла
+        char        _status;    //Статус файла 0 - добавлен, 1 - изменен, 2 - удален (exmpl: _status = 0;)
     };
 
     struct DirectoryInfo //Структура содержащая информацию о конкретной директории
     {
-        char* _dirname; //Имя директории в строковом виде
-        int32_t     _level, //уровень дириктории от -1 (/)
-                    _ID_on_level, //ID на уровне
-                    _count_child_IDs, //количество дочерних директорий и их ID
-                    _count_FI; //Количество фалов в дириктории
-        DirectoryInfo* _child_IDs_arr; //Массив указателей на дочерние дириктории
-        FileInfo* _FileInfoArr; //Массив указателей на файлы содержащиеся в каталоги
+        char*           _dirname;       //Имя директории в строковом виде
+        int32_t         _level;         //уровень дириктории от -1 (/)
+        uint32_t        _ID,            //ID на уровне
+                        _countChild,    //количество дочерних директорий и их ID
+                        _countFI;       //Количество фалов в дириктории
+        DirectoryInfo*  _childID;       //Массив указателей на дочерние дириктории
+        FileInfo*       _FI;            //Массив указателей на файлы содержащиеся в каталоги
     };
 
     struct StateTree //Структура вершины древа состояния
     {
-        StateTree* _DaughterStateTreeArr; //Массив указателей на дочерние вершины дерева
-        FileInfo* _FilesArr; //Массив указателей на файлы содержащиеся в данной вершине
-        int32_t     _DaugtersCount, //Количество дочерних вершин
-                    _FileCount; //Количество файлов в вершине
+        StateTree*      _child;         //Массив указателей на дочерние вершины дерева
+        uint32_t*       _ID;            //Массив ID файлов в вершине
+        uint32_t        _countDirs,     //Количество дочерних вершин
+                        _countFiles;    //Количество файлов в вершине
+    };
+
+    struct Modification //Структура определяющая модификацию
+    {
+        char*       _author,        //Имя автора модификации
+                    _countFiles,    //Количество файлов в модификации
+                    _message;       //Сообщений с комментарием к модификации
+        uint32_t    _date;          //Дата произведения модификации
+        FileInfo*   _file;          //Массив указателей на файлы в модификации
     };
 
     class ModificationManager
     {
     public:
-        void GetInitialState(); //Метод получающий начальное состояние репозитория
-        void GetNextModification(); //Метод получающий последующие изменения состояния репозитория
-        DirectoryInfo* _DirectoryInfoArr; //Массив указателей на директории
-        FileInfo* _FileInfoArr; //Массив указателей на файлы
-        void SaveToFile(); //Метод сохраняющий структуры Modification в файл...
-        void LoadFromFile(); //...и метод их загружающий из файла
+        void GetInitialState();                         //Метод получающий начальное состояние репозитория
+        void GetNextModification();                     //Метод получающий последующие изменения состояния репозитория
+        void SaveToFile(const char* filename);          //Метод сохраняющий структуры Modification в файл...
+        void LoadFromFile(const char* filename);        //...и метод их загружающий из файла
+
+    private:
+        DirectoryInfo*  _DirectoryInfoArr;  //Массив указателей на директории
+        FileInfo*       _FileInfoArr;       //Массив указателей на файлы
+
     };
 }
 
