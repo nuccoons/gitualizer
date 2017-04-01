cc0b294cb0db73e1983eb04177854546d060a867
nuccoons <nuccoons@gmail.com>
2017-04-01 16:32:45 +0300

    Add files via upload

--- include/core/core_common.hpp
+++ include/core/core_common.hpp
@@ -1,34 +1,12 @@
 #ifndef CORE_COMMON_HPP
 #define CORE_COMMON_HPP
 
-#include <cstdint>
+#include "../common/common.hpp"
 
 namespace gitualizer
 {
 	namespace core
 	{
-		enum FileStatus
-		{
-			ADDED = 1,
-			MODIFIED = 2,
-			REMOVED = 4
-		};
-
-		struct File //Данные файла полученые из репозитория
-		{
-			char*		_filename;	//Имя файла
-			FileStatus	_status;	//Статус файла 1 - добавлен, 2 - изменен, 4 - удален
-		};
-
-		struct Modification //Структура определяющая модификацию
-		{
-			char*		_author;		//Имя автора модификации
-			char*		_message;		//Сообщений с комментарием к модификации
-			char*		_date;			//Дата произведения модификации
-			File*		_files;			//Массив указателей на файлы в модификации
-			uint32_t	_countFiles;	//Количество файлов в модификации
-		};
-
 		struct FileInfo //Структа содержащая информацию о конкретном файле
 		{
 			char*		_filename;	//Имя файла в строковом виде
