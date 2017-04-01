20f7e17ed77a202b59871aa80b907cf89cc7ff24
pspirichev <pspirichev@yandex.ru>
2017-04-01 15:03:44 +0400

    Add files via upload

--- include/core/modification_manager.hpp
+++ include/core/modification_manager.hpp
@@ -2,22 +2,25 @@
 #define MODIFICATION_MANAGER_HPP
 
 #include <cstdint>
-#include <common.hpp>
+#include "../common/common.hpp"
 
 namespace gitualizer
 {
-    class ModificationManager
+    namespace core
     {
-    private:
-        Modification*   _cacheModification;   //Массив указателей на модификация (кэш мдифифкаций)
-        
-    public:
-        Modification GetInitialState() const;               //Метод получающий начальное состояние репозитория
-        Modification GetNextModification() const;           //Метод получающий последующие изменения состояния репозитория
-        FileInfo GetFileInfoById(uint32_t id) const;        //Метод возвращающий информацию о файле по его ID
-        int SaveToFile(const char* filename);               //Метод сохраняющий структуры Modification в файл...
-        int LoadFromFile(const char* filename);             //...и метод их загружающий из файла
-    };
+        class ModificationManager
+        {
+        private:
+            Modification*   cacheModification;   //Массив указателей на модификация (кэш мдифифкаций)
+
+        public:
+            Modification GetInitialState();             //Метод получающий начальное состояние репозитория
+            Modification GetNextModification();         //Метод получающий последующие изменения состояния репозитория
+            FileInfo GetFileInfoById(uint32_t id) const;//Метод возвращающий информацию о файле по его ID
+            int SaveToFile(const char* filename);       //Метод сохраняющий структуры Modification в файл...
+            int LoadFromFile(const char* filename);     //...и метод их загружающий из файла
+        };
+    }
 }
 
 #endif // MODIFICATION_MANAGER_HPP
--- include/core/state_manager.hpp
+++ include/core/state_manager.hpp
@@ -2,37 +2,40 @@
 #define STATE_MANAGER_HPP
 
 #include <cstdint>
-#include "common.hpp"
+#include "../common/common.hpp"
 
 namespace gitualizer
 {
-    struct StateTree //Структура вершины древа состояния
+    namespace core
     {
-        StateTree*      _child;         //Массив указателей на дочерние вершины дерева
-        uint32_t*       _ID;            //Массив ID файлов в вершине
-        uint32_t        _countDirs;     //Количество дочерних вершин
-        uint32_t        _countFiles;    //Количество файлов в вершине
-    };
-    
-    class StateManager
-    {
-    private:
-        struct DirectoryInfo //Структура содержащая информацию о конкретной директории
+        struct StateTree //Структура вершины древа состояния
         {
-            char*           _dirname;       //Имя директории в строковом виде
-            DirectoryInfo*  _dirsInfo;      //Массив указателей на дочерние дириктории
-            FileInfo*       _filesInfo;     //Массив указателей на файлы содержащиеся в каталоги
-            int32_t         _level;         //уровень дириктории от -1 (/)
-            uint32_t        _ID;            //ID на уровне
-            uint32_t        _countDirs;     //количество дочерних директорий и их ID
-            uint32_t        _countFiles;    //Количество фалов в дириктории
+            StateTree*      _child;         //Массив указателей на дочерние вершины дерева
+            uint32_t*       _ID;            //Массив ID файлов в вершине
+            uint32_t        _countDirs;     //Количество дочерних вершин
+            uint32_t        _countFiles;    //Количество файлов в вершине
         };
-        DirectoryInfo*  _dirsInfo;  //Массив указателей на директории
-   
-    public:
-        void AddModification(const Modification*);  //Метод, добавляющий Modification в StateTree
-        StateTree const* GetStateTree() const;      //Метод, получаюший StateTree
-    };
+
+        class StateManager
+        {
+        private:
+            struct DirectoryInfo //Структура содержащая информацию о конкретной директории
+            {
+                char*           _dirname;       //Имя директории в строковом виде
+                DirectoryInfo*  _dirsInfo;      //Массив указателей на дочерние дириктории
+                FileInfo*       _filesInfo;     //Массив указателей на файлы содержащиеся в каталоги
+                int32_t         _level;         //уровень дириктории от -1 (/)
+                uint32_t        _ID;            //ID на уровне
+                uint32_t        _countDirs;     //количество дочерних директорий и их ID
+                uint32_t        _countFiles;    //Количество фалов в дириктории
+            };
+            DirectoryInfo*  _dirsInfo;  //Массив указателей на директории
+
+        public:
+            void AddModification(const Modification*);  //Метод, добавляющий Modification в StateTree
+            const StateTree* GetStateTree() const;      //Метод, получаюший StateTree
+        };
+    }
 }
 
 #endif // STATE_MANAGER_HPP
